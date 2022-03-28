7fb8ae9f6561f8da892e396a3493209193cc571d
saurabhpatil1511 <101984622+saurabhpatil1511@users.noreply.github.com>
2022-03-28 11:54:43 +0530

    Add files via upload
    Assignment 3 Q1 program to add all numbers in list using function

---
+++ add all numbers using function in the list Assignment 3 Q1.py
@@ -0,0 +1,13 @@
+# Program to add all numbers in the list using function:
+def sum_numbers(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+numbers =[8,2,3,0,7]
+print(sum_numbers(numbers))
+    
+
+
+
+
